a096538ecd38d832612ba4cd5778f4a8f325b9bc
ismail dundar <dundar.sml@gmail.com>
2021-01-30 13:26:32 +0100

    added line 3

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -3,8 +3,9 @@ package com.company;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, This is IntelliJ.");
-        // line 1
+        // line 1                         // Origin/ master represents the remote area
         // line 2
+        // line 3
     }
 
 
